86367861920432b19ca6801d22154924cce972d1
christianahOlude <oludechristianah16@gmail.com>
2025-08-20 14:14:33 +0100

    responsiveness on desktop and mobile

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "tailwindcss";
-import upskill from "../assets/UpskillAfricaLogoIcon.png";
+import upskill from "../assets/Upskill-Logo.png";
 import styles from "../css/navbar.module.css";
 
 export default function Navbar() {
-    return (
-          <div className={styles.nav}>
-            <div className={styles.navbar}>
-                <img src={upskill} alt="UpskillAfricaLogoIcon"/>
-                <p>Upskill Africa</p>
+
+        const [isMenuOpen, setIsMenuOpen] = useState(false);
+            const toggleMenu = () => {
+            setIsMenuOpen(!isMenuOpen)
+        };
+            return (
+              <div className={styles.navbar}>
+                    <div >
+                        <img src={upskill} alt="Upskill-LogoIcon"/>
+                    </div>
+                 <div className={`${styles.links} ${isMenuOpen ? styles.open : ''}`}>
+                      <a href="#">Home</a>
+                      <a href="#">About</a>
+                      <a href="#">Community</a>
+                      <a href="#">Contact</a>
+                  </div>
+                  <div style={{gap: '8px', display: 'flex'}}>
+                      <button style={{backgroundColor: '#f1f5f9'}} className={styles.btn}>Join Community</button>
+                      <button style={{ backgroundColor:'#34D399', color: 'white'}} className={styles.btn}>Start Learning</button>
+                  </div>
             </div>
-        </div>
     )
 }
