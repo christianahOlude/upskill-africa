d75acac1f9ac451c270889c2a9092701d7743283
christianahOlude <oludechristianah16@gmail.com>
2025-08-22 11:50:43 +0100

    responsiveness on desktop and mobile

--- src/components/impactSection.jsx
+++ src/components/impactSection.jsx
@@ -11,18 +11,20 @@ import styles from '../css/impactSection.module.css';
 export default function Impact() {
   return (
     <section className={styles.impactSection}>
-      <div className={styles.headingWrapper}>
-        <img src={impact} alt="impact" className={styles.impactIcon} />
-        <h2 className={styles.heading}>
-          How We're Making <br />
-          an <span>Impact</span>
-        </h2>
-      </div>
+        <div style={{display: 'flex', justifyContent: 'center', padding: '50px 0'}}>
+
+            <img src={impact} alt="impact" className={styles.impactIcon} />
+            <h2 className={styles.heading}>
+              How We're Making <br />
+              an <span>Impact</span>
+            </h2>
+
+        </div>
 
       <div className={styles.grid}>
         {/* First column */}
-        <div className={styles.card}>
-          <div className={styles.cardText}>
+        <div >
+          <div className={styles.card}>
             <img src={handsOn} alt="handsOn" className={styles.smallIcon} />
             <p className={styles.cardTitle}>
               Physical Meetups & Virtual <br />
@@ -36,7 +38,7 @@ export default function Impact() {
           <img src={meetups} alt="meetups" className={styles.cardImage} />
         </div>
 
-        {/* Second column */}
+        {/*Second column*/}
         <div className={styles.card}>
           <img src={community} alt="community" className={styles.cardImage} />
           <img src={programs} alt="programs" className={styles.smallIcon} />
@@ -44,11 +46,10 @@ export default function Impact() {
             University & Tech Community <br /> Programs
           </p>
         </div>
-
-        {/* Third column */}
-        <div className={styles.card}>
+        {/*Third column*/}
+          <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '20px'}}>
           <img src={placement} alt="placement" className={styles.cardImage} />
-          <div className={styles.cardText}>
+          <div className={styles.card}>
             <img src={support} alt="support" className={styles.smallIcon} />
             <p className={styles.cardTitle}>Job Placement Support</p>
             <p className={styles.cardDesc}>
@@ -56,7 +57,7 @@ export default function Impact() {
               Blockchain industry.
             </p>
           </div>
-        </div>
+          </div>
       </div>
     </section>
   );
