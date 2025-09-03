f890b7238b62657536f45fd14cccfcc262aa9ee8
christianahOlude <oludechristianah16@gmail.com>
2025-09-03 12:57:54 +0100

    adjusted the hero-section

--- src/components/web3Section.jsx
+++ src/components/web3Section.jsx
@@ -1,30 +1,40 @@
-import React from 'react';
+import React from "react";
 import underline from "../assets/underline.png";
 import man from "../assets/man.png";
-import styles from '../css/web3Section.module.css';
+import styles from "../css/web3Section.module.css";
 import curl from "../assets/curl.png";
 
 export default function Web3Section() {
   return (
-      <div>
-        <div className={styles.web3Section}>
-          <div className={styles.web3Content}>
-            <h2 style={{fontFamily: "Helvetica, Arial, sans-serif"}}>Why <span>Web3</span></h2>
-            <img src={underline} alt="underline" />
-            <p style={{fontFamily: "Helvetica, Arial, sans-serif", fontSize: "18px",color: "#2C2C2C",lineHeight: "1.6",fontWeight: "400"}}>
-              Web3 is still in its early stages, and this is Africa’s <br />
-              opportunity to be early adopters—not just followers. <br />
-              Unlike Web2, where Africa lagged behind, Web3 presents <br />
-              a level playing field where African talent can thrive, <br />
-              create, and earn.
-            </p>
-            <button>Explore Web3</button>
-          </div>
-          <div className={styles.web3Image}>
-            <img src={man} alt="man" />
-          </div>
+    <div>
+      <div className={styles.web3Section}>
+        <div className={styles.web3Content}>
+          <h2 style={{ fontFamily: "Helvetica, Arial, sans-serif" }}>
+            Why <span>Web3</span>
+          </h2>
+          <img src={underline} alt="underline" />
+          <p
+            style={{
+              fontFamily: "Helvetica, Arial, sans-serif",
+              fontSize: "18px",
+              color: "#2C2C2C",
+              lineHeight: "1.6",
+              fontWeight: "400",
+            }}
+          >
+            Web3 is still in its early stages, and this is Africa’s <br />
+            opportunity to be early adopters—not just followers. <br />
+            Unlike Web2, where Africa lagged behind, Web3 presents <br />
+            a level playing field where African talent can thrive, <br />
+            create, and earn.
+          </p>
+          <button>Explore Web3</button>
         </div>
-          <img src={curl} alt="curl" width={480} style={{marginRight: "-50%"}}/>
+        <div className={styles.web3Image}>
+          <img src={man} alt="man" />
+        </div>
       </div>
+       <img src={curl} alt="curl" width={480} style={{ marginRight: "-30%" }} />
+    </div>
   );
 }
